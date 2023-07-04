937c44762ef806b9fce14b9e61537a022d80beab
Mutiullahi <ahmadmutiullahi@gmail.com>
2023-07-04 23:03:00 +0100

    simple commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector(".toggle");
+let toogle = document.querySelector(".nav-link")
+function opennav(){
+    toogle.classList.add("open")
+}
+function closenav(){
+    toogle.classList.remove("open")
+}
