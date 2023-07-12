0c83e3132fa322c1022bedb3af829d88ad03e5d8
Mutiullahi <ahmadmutiullahi@gmail.com>
2023-07-12 06:40:41 +0100

    seventh commit

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function opennav(){
 function closenav(){
     toogle.classList.remove("open")
 }
+// sending form directly to the email
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "ahmadmutiullahi@gmail.com",
+        Password : "168C6E225692D3AEBEC363A6E34D26EA2198",
+        To : 'ahmadmutiullahi@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "FOR OFFICIAL ONLY",
+        Body : "Name :" + document.getElementById("name").value
+                + " <br> Email :" + document.getElementById("email").value
+                +" <br> message :" + document.getElementById("message").value
+    }).then(
+      message => alert(message)
+    );
+}
