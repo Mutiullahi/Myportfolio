1ed414a076e7f04e7577a8237e1921980aa76e00
Mutiullahi Ahmad <ahmadmutiullahi@gmail.com>
2023-11-06 02:09:06 -0800

    new project

--- script.js
+++ script.js
@@ -61,3 +61,28 @@ function openpopup(){
 function closepopup(){
     popup.classList.remove("openpopup")
 }
+
+// for search items
+
+const search = ()=> {
+    const searchbox = document.getElementById("search-items").value.toUpperCase();
+    const storeitems = document.getElementById("products-list");
+    const product = document.querySelectorAll(".product");
+    const pname = document.getElementsByTagName("p");
+
+    for (var i = 0; i < pname.length; i++){
+        let match = product[i].getElementsByTagName('p')[0];
+
+        if (match){
+            let textvalue = match.textContent ||  match.innerHTML
+
+            if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+                product[i].style.display ="";
+                
+            }else{
+                product[i].style.display ="none";
+                
+            }
+        }
+    }
+}
